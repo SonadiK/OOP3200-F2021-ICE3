a3440bc94cb61dd7cd3d29872936192951ec4056
Sonadi Kannangara <ksonadi@gmail.com>
2021-09-27 22:56:35 -0400

    Add project files.

---
+++ OOP3200-F2021-ICE3/Professor.cpp
@@ -0,0 +1,51 @@
+/**
+ * Project OOP3200-F2021_ICE3
+ * @author Sonadi Kannangara
+ * @version 1.0
+ */
+
+
+#include "Professor.h"
+
+/**
+ * Professor implementation
+ */
+
+
+/**
+ * @param first_name
+ * @param last_name
+ * @param age
+ * @param employee_id
+ */
+void Professor::Professor(string first_name, string last_name, float age, string employee_id) {
+
+}
+
+/**
+ * @return string
+ */
+string Professor::getEmployeeID() {
+    return "";
+}
+
+/**
+ * @param value
+ */
+void Professor::setEmployeeID(string value) {
+
+}
+
+/**
+ * @return void
+ */
+void Professor::Teacher() {
+    return;
+}
+
+/**
+ * @return string
+ */
+string Professor::ToString() {
+    return "";
+}
---
+++ OOP3200-F2021-ICE3/main.cpp
@@ -0,0 +1,10 @@
+// OOP3200-F2021-ICE3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+   
+}
+
